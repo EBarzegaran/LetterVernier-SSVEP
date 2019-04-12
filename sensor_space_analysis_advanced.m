1debe1a4eaf69d4c48373e310d2af15d1aedc239
EBarzegaran <e.barzegaran@gmail.com>
2019-04-12 16:40:51 -0700

    minor updates

--- sensor_space_analysis_advanced.m
+++ sensor_space_analysis_advanced.m
@@ -42,12 +42,6 @@ for Sub = 1:numel(SubIDs)
 end
 
 
-%% Group Level RCA
-fRCA = 7;
-[decompAxx_Letter_all,~,A_Letter_all,D_Letter_all] = mrC.SpatialFilters.RCA(MergeAxx(axx_Letter),'freq_range',Freqs(fRCA));%,
-[decompAxx_Vernier_all,~,A_Vernier_all,D_Vernier_all] = mrC.SpatialFilters.RCA(MergeAxx(axx_Vernier),'freq_range',Freqs(fRCA));
-
-
 %% plot individual and average ASD results
 
 Mode = 'Amp';%'Phase'
@@ -109,27 +103,38 @@ for Sub = 17: numel(Sublist)
          close all;
     end
 end
- 
+
+%% GROUP LEVEL ANALYSIS
+%SELECT the harmonic to do analysis 
+analFreq = 1;
+%%%%%%
+%% Group Level RCA
+
+fRCA = Finds(analFreq);
+[decompAxx_Letter_all,~,A_Letter_all,D_Letter_all] = mrC.SpatialFilters.RCA(MergeAxx(axx_Letter),'freq_range',Freqs(fRCA));%,
+[decompAxx_Vernier_all,~,A_Vernier_all,D_Vernier_all] = mrC.SpatialFilters.RCA(MergeAxx(axx_Vernier),'freq_range',Freqs(fRCA)); 
+
 %% Plot RC components calculated on all subjects
 
 % figure params
 Cols = brewermap(2,'Set2');
 
-
-caxisR2 = [-1.1 1.1];
-caxisR1 = caxisR2;
+Vinv = 1;
+Linv = 1;
+caxisRV = [-1*max(max(A_Vernier_all(:,1:2))) max(max(A_Vernier_all(:,1:2)))];
+caxisRL = [-1*max(max(A_Letter_all(:,1:2))) max(max(A_Letter_all(:,1:2)))];
 NCOMP  = 2;
 FIG=figure;
 set(FIG,'unit','inch','position',[17 10 8 14])
 set(FIG,'unit','inch','paperposition',[17 10 8 14])
 for comp = 1:NCOMP 
-    subplot(4,2,comp*2),mrC.Simulate.plotOnEgi(A_Vernier_all(:,comp)*-1);axis tight equal;caxis(caxisR2);
+    subplot(4,2,comp*2),mrC.Simulate.plotOnEgi(A_Vernier_all(:,comp)*Vinv);axis tight equal;caxis(caxisRV);
     title(['RC' num2str(comp)],'fontsize',FS,'Color',Cols(comp,:));%,'fontweight','normal');%colorbar;
     if comp==1
         text(-.4, 2,'Vernier','fontsize',FS+4,'fontweight','bold')
     end
     
-    subplot(4,2,comp*2-1),mrC.Simulate.plotOnEgi(A_Letter_all(:,comp));axis tight equal;caxis(caxisR2);
+    subplot(4,2,comp*2-1),mrC.Simulate.plotOnEgi(A_Letter_all(:,comp)*Linv);axis tight equal;caxis(caxisRL);
     title(['RC' num2str(comp)],'fontsize',FS,'Color',Cols(comp,:));%,'fontweight','normal');%colorbar;
     if comp==1
         text(-.4, 2,'Letter','fontsize',FS+4,'fontweight','bold')
@@ -139,20 +144,20 @@ end
 Subnum = numel(SubIDs);
 Condnum = 5;
 nF1 = fRCA;
-Ver_cos = squeeze(mean(reshape(squeeze(decompAxx_Vernier_all.Cos(nF1,:,1:end-70)),[128,16,Condnum,Subnum-1]),2));% last subject has 14 trails
-Ver_sin = squeeze(mean(reshape(squeeze(decompAxx_Vernier_all.Sin(nF1,:,1:end-70)),[128,16,Condnum,Subnum-1]),2));
+Ver_cos = squeeze(mean(reshape(squeeze(decompAxx_Vernier_all.Cos(nF1,:,1:end-70)),[numel(D_Vernier_all),16,Condnum,Subnum-1]),2));% last subject has 14 trails
+Ver_sin = squeeze(mean(reshape(squeeze(decompAxx_Vernier_all.Sin(nF1,:,1:end-70)),[numel(D_Vernier_all),16,Condnum,Subnum-1]),2));
 
-Ver_cos(:,:,Subnum) = squeeze(mean(reshape(squeeze(decompAxx_Vernier_all.Cos(nF1,:,end-69:end)),[128,14,Condnum,1]),2));
-Ver_sin(:,:,Subnum) = squeeze(mean(reshape(squeeze(decompAxx_Vernier_all.Sin(nF1,:,end-69:end)),[128,14,Condnum,1]),2));
-Ver_cmplx = -1*Ver_cos-(Ver_sin*1i);
+Ver_cos(:,:,Subnum) = squeeze(mean(reshape(squeeze(decompAxx_Vernier_all.Cos(nF1,:,end-69:end)),[numel(D_Vernier_all),14,Condnum,1]),2));
+Ver_sin(:,:,Subnum) = squeeze(mean(reshape(squeeze(decompAxx_Vernier_all.Sin(nF1,:,end-69:end)),[numel(D_Vernier_all),14,Condnum,1]),2));
+Ver_cmplx = Vinv*Ver_cos+(Ver_sin*1i*Vinv);
 
 
-Let_cos = squeeze(mean(reshape(squeeze(decompAxx_Letter_all.Cos(nF1,:,1:end-70)),[128,16,Condnum,Subnum-1]),2));% last subject has 14 trails
-Let_sin = squeeze(mean(reshape(squeeze(decompAxx_Letter_all.Sin(nF1,:,1:end-70)),[128,16,Condnum,Subnum-1]),2));
+Let_cos = squeeze(mean(reshape(squeeze(decompAxx_Letter_all.Cos(nF1,:,1:end-70)),[numel(D_Letter_all),16,Condnum,Subnum-1]),2));% last subject has 14 trails
+Let_sin = squeeze(mean(reshape(squeeze(decompAxx_Letter_all.Sin(nF1,:,1:end-70)),[numel(D_Letter_all),16,Condnum,Subnum-1]),2));
 
-Let_cos(:,:,Subnum) = squeeze(mean(reshape(squeeze(decompAxx_Letter_all.Cos(nF1,:,end-69:end)),[128,14,Condnum,1]),2));
-Let_sin(:,:,Subnum) = squeeze(mean(reshape(squeeze(decompAxx_Letter_all.Sin(nF1,:,end-69:end)),[128,14,Condnum,1]),2));
-Let_cmplx = Let_cos+(Let_sin*1i);
+Let_cos(:,:,Subnum) = squeeze(mean(reshape(squeeze(decompAxx_Letter_all.Cos(nF1,:,end-69:end)),[numel(D_Letter_all),14,Condnum,1]),2));
+Let_sin(:,:,Subnum) = squeeze(mean(reshape(squeeze(decompAxx_Letter_all.Sin(nF1,:,end-69:end)),[numel(D_Letter_all),14,Condnum,1]),2));
+Let_cmplx = Linv*Let_cos+(Let_sin*1i*Linv);
 
 for comp = 1:NCOMP 
     subplot(4,2,6), %plot(logMAR_ver,mean(abs(Ver_cmplx(comp,:,:)),3),'-o','Color',Cols(comp,:),'linewidth',1.5);hold on;
@@ -185,96 +190,34 @@ for comp = 1:NCOMP
     ylabel('1F1 Phase degrees')
 end
 
-print(FIG,['../Presentation/TopoMap_individuals/RCA_1f1_AverageAll'],'-r300','-dtiff');
+print(FIG,['../Presentation/TopoMap_individuals/RCA_' Harms{Finds} '_AverageAll'],'-r300','-dtiff');
 close all;
-%% Plot group level RCs and their 2D phase plots
-Subnum = numel(SubIDs);
-Condnum = 5;
-nF1 = fRCA;
-Ver_cos = squeeze(mean(reshape(squeeze(decompAxx_Vernier_all.Cos(nF1,:,1:end-70)),[128,16,Condnum,Subnum-1]),2));% last subject has 14 trails
-Ver_sin = squeeze(mean(reshape(squeeze(decompAxx_Vernier_all.Sin(nF1,:,1:end-70)),[128,16,Condnum,Subnum-1]),2));
 
-Ver_cos(:,:,Subnum) = squeeze(mean(reshape(squeeze(decompAxx_Vernier_all.Cos(nF1,:,end-69:end)),[128,14,Condnum,1]),2));
-Ver_sin(:,:,Subnum) = squeeze(mean(reshape(squeeze(decompAxx_Vernier_all.Sin(nF1,:,end-69:end)),[128,14,Condnum,1]),2));
-
-
-Let_cos = squeeze(mean(reshape(squeeze(decompAxx_Letter_all.Cos(nF1,:,1:end-70)),[128,16,Condnum,Subnum-1]),2));% last subject has 14 trails
-Let_sin = squeeze(mean(reshape(squeeze(decompAxx_Letter_all.Sin(nF1,:,1:end-70)),[128,16,Condnum,Subnum-1]),2));
-
-Let_cos(:,:,Subnum) = squeeze(mean(reshape(squeeze(decompAxx_Letter_all.Cos(nF1,:,end-69:end)),[128,14,Condnum,1]),2));
-Let_sin(:,:,Subnum) = squeeze(mean(reshape(squeeze(decompAxx_Letter_all.Sin(nF1,:,end-69:end)),[128,14,Condnum,1]),2));
-
-Colors = {'k','g','b'};
-FIG=figure;
-for cond = 1:Condnum
-    subplot(5,2,2*cond);
-    for comp = 1:2
-        if comp==1
-            l(comp) = line([0 mean(Ver_cos(comp,cond,:),3)]*-1,[0 mean(Ver_sin(comp,cond,:),3)]*-1,'color',Colors{comp},'LineWidth',1.5);hold on;
-            [ampDiff,phaseDiff,zSNR,errorEllipse] = fitErrorEllipse([squeeze(Ver_cos(comp,cond,:)) squeeze(Ver_sin(comp,cond,:))]*-1,'SEM');
-        else
-            l(comp) = line([0 mean(Ver_cos(comp,cond,:),3)],[0 mean(Ver_sin(comp,cond,:),3)],'color',Colors{comp},'LineWidth',1.5);hold on;
-            [ampDiff,phaseDiff,zSNR,errorEllipse] = fitErrorEllipse([squeeze(Ver_cos(comp,cond,:)) squeeze(Ver_sin(comp,cond,:))],'95CI');
-        end
-        
-        plot(errorEllipse(:,1), errorEllipse(:,2),'k-','LineWidth',1.5,'color',Colors{comp}) ;
-    end
-    if cond ==1
-        title('Vernier');
-        legend(l,{'comp1','comp2'});
-    end
-    xlim([-1 1]);
-    ylim([-1 1]);
-end
-
-Let_cos = squeeze(mean(reshape(squeeze(decompAxx_Letter_all.Cos(nF1,:,1:end-70)),[128,16,Condnum,Subnum-1]),2));% last subject has 14 trails
-Let_sin = squeeze(mean(reshape(squeeze(decompAxx_Letter_all.Sin(nF1,:,1:end-70)),[128,16,Condnum,Subnum-1]),2));
-
-Let_cos(:,:,Subnum) = squeeze(mean(reshape(squeeze(decompAxx_Letter_all.Cos(nF1,:,end-69:end)),[128,14,Condnum,1]),2));
-Let_sin(:,:,Subnum) = squeeze(mean(reshape(squeeze(decompAxx_Letter_all.Sin(nF1,:,end-69:end)),[128,14,Condnum,1]),2));
-
-for cond = 1:5
-    subplot(5,2,2*cond-1);
-    for comp = 1:2
-        l(comp) = line([0 mean(Let_cos(comp,cond,:),3)],[0 mean(Let_sin(comp,cond,:),3)],'color',Colors{comp},'LineWidth',1.5);hold on;
-        [ampDiff,phaseDiff,zSNR,errorEllipse] = fitErrorEllipse([squeeze(Let_cos(comp,cond,:)) squeeze(Let_sin(comp,cond,:))],'95CI');
-        plot(errorEllipse(:,1), errorEllipse(:,2),'k-','LineWidth',1.5,'color',Colors{comp}) ;
-    end   
-    if cond ==1
-        title('Letter')
-        legend(l,{'comp1','comp2'});
-    end
-    xlim([-1 1]);
-    ylim([-1 1]);
-end
-
-set(gcf,'unit','inch','position',[9 10 8 15]);
-%print(FIG,['../Presentation/RCA_phase_' num2str((fRCA-1)/6) 'f1'],'-r300','-dtiff');
 %% Plot individual-level RCs 
-sublist = [1:16];
-figure;
-for s = 1:numel(sublist)
-    sub = sublist(s);
-    subplot(2,numel(sublist),s), mrC.Simulate.plotOnEgi(A_Letter{sub}(:,1)); axis tight;
-    M = max(max(abs(A_Letter{sub}(:,1:2))));
-    caxis([-M M]);
-    title(SubIDs{sub})
-    subplot(2,numel(sublist),s+numel(sublist)), mrC.Simulate.plotOnEgi(A_Letter{sub}(:,2)); axis tight
-    caxis([-M M]);
-end
-set(gcf,'unit','inch','position',[5 10 34 4.5])
-
-figure;
-for s = 1:numel(sublist)
-    sub = sublist(s);
-    subplot(2,numel(sublist),s), mrC.Simulate.plotOnEgi(A_Vernier{sub}(:,1)); axis tight 
-    M = max(max(abs(A_Vernier{sub}(:,1:2))));
-    caxis([-M M]);
-    title(SubIDs{sub})
-    subplot(2,numel(sublist),s+numel(sublist)), mrC.Simulate.plotOnEgi(A_Vernier{sub}(:,2)); axis tight
-    caxis([-M M]);
-end
-set(gcf,'unit','inch','position',[5 5 34 4.5])
+% sublist = [1:16];
+% figure;
+% for s = 1:numel(sublist)
+%     sub = sublist(s);
+%     subplot(2,numel(sublist),s), mrC.Simulate.plotOnEgi(A_Letter{sub}(:,1)); axis tight;
+%     M = max(max(abs(A_Letter{sub}(:,1:2))));
+%     caxis([-M M]);
+%     title(SubIDs{sub})
+%     subplot(2,numel(sublist),s+numel(sublist)), mrC.Simulate.plotOnEgi(A_Letter{sub}(:,2)); axis tight
+%     caxis([-M M]);
+% end
+% set(gcf,'unit','inch','position',[5 10 34 4.5])
+% 
+% figure;
+% for s = 1:numel(sublist)
+%     sub = sublist(s);
+%     subplot(2,numel(sublist),s), mrC.Simulate.plotOnEgi(A_Vernier{sub}(:,1)); axis tight 
+%     M = max(max(abs(A_Vernier{sub}(:,1:2))));
+%     caxis([-M M]);
+%     title(SubIDs{sub})
+%     subplot(2,numel(sublist),s+numel(sublist)), mrC.Simulate.plotOnEgi(A_Vernier{sub}(:,2)); axis tight
+%     caxis([-M M]);
+% end
+% set(gcf,'unit','inch','position',[5 5 34 4.5])
 
 
 %% functions
